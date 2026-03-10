d6191137d44f8dd8af22518f615bc6a65400d9b9
Arzy4 <robbyarzy@gmail.com>
2026-03-10 20:25:24 +0700

    Update index.html and style.css, Add Carousel effect for projects card and skills & tools

--- index.js
+++ index.js
@@ -1,3 +1,126 @@
+// skill & tools slider
+const stContainer = document.querySelector(".st-container2");
+const stSlides = document.querySelectorAll(".st-container2 > div");
+const stMobile = window.matchMedia("(max-width:768px)");
+
+let stIndex = 0;
+let stStartX = 0;
+let stEndX = 0;
+let stAutoplay;
+
+function showSkillSlide(i){
+    stContainer.style.transform = `translateX(-${i * 100}%)`;
+}
+
+function nextSkillSlide(){
+    stIndex++;
+    if(stIndex >= stSlides.length){
+        stIndex = 0;
+    }
+    showSkillSlide(stIndex);
+}
+
+// autoplay
+function checkSkillSlider(){
+    clearInterval(stAutoplay);
+
+    if(stMobile.matches){
+        stAutoplay = setInterval(nextSkillSlide,3000);
+    } else {
+        stContainer.style.transform = "translateX(0)";
+        stIndex = 0;
+    }
+}
+
+checkSkillSlider();
+stMobile.addEventListener("change", checkSkillSlider);
+
+// swipe gesture
+stContainer.addEventListener("touchstart", (e) => {
+    stStartX = e.touches[0].clientX;
+});
+
+stContainer.addEventListener("touchend", (e) => {
+    stEndX = e.changedTouches[0].clientX;
+
+    if(stStartX - stEndX > 50){
+        stIndex++;
+    } else if (stEndX - stStartX > 50){
+        stIndex--;
+    }
+
+    if(stIndex >= stSlides.length) stIndex = 0;
+    if(stIndex < 0) stIndex = stSlides.length - 1;
+
+    showSkillSlide(stIndex);
+
+    clearInterval(stAutoplay);
+    stAutoplay = setInterval(nextSkillSlide,3000);
+});
+
+// project card slider
+const pContainer = document.querySelector(".projects-container");
+const pSlides = document.querySelectorAll(".projects-container article");
+const pMobile = window.matchMedia("(max-width:900px)");
+
+let pIndex = 0;
+let pStartX = 0;
+let pEndX = 0;
+let pAutoplay;
+
+function showSlide(i){
+    pContainer.style.transform = `translateX(-${i * 100}%)`;
+}
+
+function nextSlide(){
+    console.log("pAutoplay running");
+    pIndex++;
+    if(pIndex >= pSlides.length){
+        pIndex = 0;
+    }
+    showSlide(pIndex);
+}
+
+// Autoplay
+function checkSlider(){
+    clearInterval(pAutoplay);
+
+    if(pMobile.matches){
+        pAutoplay = setInterval(nextSlide,3000);
+    } else {
+        pContainer.style.transform = "translateX(0)";
+        pIndex = 0;
+    }
+}
+checkSlider();
+pMobile.addEventListener("change", checkSlider);
+
+// swipe gesture
+// touch start 
+pContainer.addEventListener("touchstart", (e) => {
+    pStartX = e.touches[0].clientX;
+});
+
+// touch end
+pContainer.addEventListener("touchend", (e) => {
+    pEndX = e.changedTouches[0].clientX;
+
+    if(pStartX - pEndX > 50){
+    pIndex++;
+    } else if (pEndX - pStartX > 50){
+        pIndex--;
+    }
+
+    if(pIndex >= pSlides.length) pIndex = 0;
+    if(pIndex < 0) pIndex = pSlides.length - 1;
+
+    showSlide(pIndex);
+
+    clearInterval(pAutoplay);
+    pAutoplay = setInterval(nextSlide, 3000);
+});
+
+// send input form data to console
 const form = document.querySelector("#form");
 const successMessage = document.querySelector("#success-message");
 
